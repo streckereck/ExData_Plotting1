4397f0d757db14feee80f12296db1642c22feb92
streckereck <colin.ferster@gmail.com>
2015-02-04 15:25:00 -0800

    Added tools to download the data, unzip, and read in
    More reproducable than doing it by hand

--- plot2.R
+++ plot2.R
@@ -1,4 +1,11 @@
-# remember to set working directory and copy data into working directory
+# set the working directory
+setwd("~/coursera/exploratoryDataAnalysis/ExData_Plotting1")
+
+#Download the file
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","temp.zip")
+
+# unzip the file
+unzip("temp.zip")
 
 # read in the  data
 mydata = read.table("household_power_consumption.txt", header = TRUE, sep=";",na.strings="?")
